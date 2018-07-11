d61ca6e0ea4bda9265b963f494b99c51f0f7b931
Khadiza, Bibi <bibi.khadiza@accenture.com>
2018-07-11 17:49:51 -0400

    integrated post function for node server

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import { Employee } from '../models/employee.model';
+import { FormPoster } from '../services/form-poster.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'home',
@@ -11,6 +13,8 @@ export class HomeComponent {
   model = new Employee('Darla', 'smith', true, "W2", "default");
   hasPrimaryLanguageError = false;
 
+  constructor(private formPoster: FormPoster){}
+
   validatePrimaryLanguage(event){
     console.log("lang:", this.model.primaryLanguage)
     if (event === 'default')
@@ -18,4 +22,17 @@ export class HomeComponent {
     else
       this.hasPrimaryLanguageError = false;
   }
+
+  submitForm(Form: NgForm) {
+    this.validatePrimaryLanguage(this.model.primaryLanguage);
+    if( this.hasPrimaryLanguageError )
+      return;
+
+    this.formPoster.postEmployeeForm(this.model)
+      .subscribe(
+        data => console.log('success: ', data),
+        err => console.log('error: ', err)
+      )
+  }
+
 }
---
+++ src/app/services/form-poster.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions} from '@angular/http';
+import { Employee } from '../models/employee.model';
+import 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
+
+
+@Injectable()
+export class FormPoster {
+
+  constructor(private http: Http) {}
+
+  postEmployeeForm(employee: Employee): Observable<any> {
+    let body = JSON.stringify(employee);
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+
+    return this.http.post('http://localhost:3100/postemployee', body, options)
+      .map(this.extractData)
+      .catch(this.handleError);
+
+  }
+
+  extractData() {
+
+  }
+
+  handleError(){
+
+  }
+
+}
